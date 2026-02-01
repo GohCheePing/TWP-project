7291dddce4ffb465fc4e30516a03dce2da4b90b9
Limziyang02 <lim.zi.yang@student.mmu.edu.my>
2026-02-01 17:14:34 +0800

    update

--- database.php
+++ database.php
@@ -6,13 +6,6 @@ $dbname = "dentaldb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-/*if ($conn){
-   echo "You are connected";
-}
-else {
-   echo "Connection failed";
-} */
-
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
