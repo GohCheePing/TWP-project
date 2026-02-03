1037daacd313b5840ee4e71ce13b95beed3806a0
GohCheePing <GOH.CHEE.PING@student.mmu.edu.my>
2026-02-03 21:25:18 +0800

    Add files via upload
    Signed-off-by: GohCheePing <GOH.CHEE.PING@student.mmu.edu.my>

--- customer.sql
+++ customer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 25, 2026 at 05:17 AM
+-- Generation Time: Feb 03, 2026 at 02:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,7 +43,10 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`Cus_ID`, `Cus_Name`, `Cus_Password`, `Cus_IC`, `Cus_Phone`, `Cus_Email`, `Register_Date`) VALUES
 (1, 'LZY', 'LZY11', '101010123', '01110336789', 'lzy1@gmail.com', '2026-01-25 00:46:51'),
-(7, 'TT', '$2y$10$FYmHSQUtYQb3N6EX9VVN8OWq0UbbnBZoA7CbgSFwLK13ZWOwSvKiG', '060717010244', '013456789', 'tt@gmail.com', '2026-01-25 04:37:04');
+(7, 'TT', '$2y$10$FYmHSQUtYQb3N6EX9VVN8OWq0UbbnBZoA7CbgSFwLK13ZWOwSvKiG', '060717010244', '013456789', 'tt@gmail.com', '2026-01-25 04:37:04'),
+(8, 'Goh Chee Ping', '$2y$10$WwnkobAup0xNDH8ajH4dbOOaAEH9mpED5VkQ2e0Lde2FUPDMwObg6', '012345678900', '01234567890', 'cheeping1212@gmail.com', '2026-02-03 20:44:06'),
+(9, 'Goh Chee Ping', '$2y$10$oCdwTUPRrpw01MtPQEdJOOkguXgiox8Erk/M/EuDRpa3N.Y5Bev.a', '012345678999', '01234567899', 'cheeping121212@gmail.com', '2026-02-03 20:46:47'),
+(10, 'Test', '$2y$10$8ffqryEfWPekx3SJSBKA7.WvzlI5SXkTA57MRh3tfF7zggwnmRw3G', '123456789012', '01201201234', 'test@gmail.com', '2026-02-03 21:19:47');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +66,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `Cus_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `Cus_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
